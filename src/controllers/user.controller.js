13bd33c128b5862f1e1d1eb49935b3a1d342d982
dpvasani <vadanidarshan@gmail.com>
2024-09-03 09:34:48 +0530

    Router And Controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+import { asyncHandler } from "...utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Register User",
+  });
+});
