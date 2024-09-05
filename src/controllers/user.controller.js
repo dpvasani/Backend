515c60c09dd1b1ece39a514c86f5e2ae4934c59b
dpvasani <vadanidarshan@gmail.com>
2024-09-05 08:42:54 +0530

    Logic Building | Register Controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,19 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Register User",
-  });
+  //   res.status(200).json({
+  //     success: true,
+  //     message: "Register User",
+  //   });
+  // Get User Details from frontend
+  // Validation that not empty
+  // Check if User Already Exists : UserName, Email
+  // Check For Avatar And Image
+  // Upload Them To Cloudnary
+  // Create User Object - Create Entry In DB
+  // Remove Password And Refresh Token From Field Of Response
+  //  Check For User Creation
+  // Check For Response
 });
 
 export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.jss";
 
 const router = Router();
 
 // Route to handle user registration
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "Avatar",
+      maxCount: 1,
+    },
+    {
+      name: "CoverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 // router.route("/login").post(login);
 // https::/localhost:8000/user/register
 
