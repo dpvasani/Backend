db4fa47e27787d0ac70d4f882773e5145b31096c
dpvasani <vadanidarshan@gmail.com>
2024-09-05 17:00:15 +0530

    Logic Building | Register Controller

--- src/index.js
+++ src/index.js
@@ -23,27 +23,3 @@ connectDB()
   .catch((err) => {
     console.log("MongoDB Connection Failed", err);
   });
-
-//   import dotenv from "dotenv";
-//   import express from "express";
-//   import connectDB from "./db/dbconnect.js";
-
-//   dotenv.config(); // This should load your .env variables
-
-//   const app = express();
-
-//   // Other imports and app setup
-//   // DB Is Connected Asynchronously So It Will Also Return Promise
-//   connectDB()
-//       .then(() => {
-//           app.on("error", (error) => {
-//               console.log("Error in Server Setup", error);
-//               throw error;
-//           });
-//           app.listen(process.env.PORT || 8000, () => {
-//               console.log(`Server is running on port ${process.env.PORT || 8000}`);
-//           });
-//       })
-//       .catch((err) => {
-//           console.log("MongoDB Connection Failed", err);
-//       });
