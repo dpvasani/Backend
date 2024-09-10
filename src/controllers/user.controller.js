c33c6d4a0ee5998d1b41ca1f9d09f693e7c49aa8
dpvasani <vadanidarshan@gmail.com>
2024-09-10 13:53:02 +0530

    Postman Testing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -44,7 +44,17 @@ const registerUser = asyncHandler(async (req, res) => {
   // Multer Getting File Local Path
   // console.log(req.files)
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    // Optional Check
+    // coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+    // Mandate Check
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar File Is Required");
   }
