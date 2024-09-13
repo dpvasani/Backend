589660fae4d6b415388904643d8fc6ec66eb88ec
dpvasani <vadanidarshan@gmail.com>
2024-09-13 19:20:09 +0530

    Subscryption Model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -3,22 +3,12 @@ import mongoose, { Schema } from "mongoose";
 const subscriptionSchema = new Schema(
   {
     subscriber: {
-      type: Schema.Types.ObjectId, // One Who Is Subscribing
+      type: Schema.Types.ObjectId, // one who is subscribing
       ref: "User",
-      required: true,
     },
     channel: {
-      type: Schema.Types.ObjectId, // One To Whom
+      type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
       ref: "User",
-      required: true,
-    },
-    subscribedAt: {
-      type: Date,
-      default: Date.now,
-    },
-    isActive: {
-      type: Boolean,
-      default: true,
     },
   },
   { timestamps: true }
