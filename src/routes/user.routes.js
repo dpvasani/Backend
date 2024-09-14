d174c803085eccefc98b4bbf6c0a8d7cc9f8bfb1
dpvasani <vadanidarshan@gmail.com>
2024-09-14 18:49:51 +0530

    All Router Works Properly And Tested On Postman

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,6 +10,7 @@ import {
   getCurrentUser,
   updateAccountDetails,
   getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { ApiError } from "../utils/ApiError.js";
@@ -43,8 +44,8 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
-router.route("/current—user").get(verifyJWT, getCurrentUser);
-router.route("/update—account").patch(verifyJWT, updateAccountDetails);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router
   .route("/avatar")
   .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
@@ -52,4 +53,5 @@ router
   .route("/cover-image")
   .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 export default router;
