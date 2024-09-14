abfaab865a6d224590a3e4df8a5f8f2c1fed4c0a
dpvasani <vadanidarshan@gmail.com>
2024-09-14 14:55:57 +0530

    Added Some Router

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,12 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  updateUserAvatar,
+  updateUserCoverImage,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  getUserChannelProfile,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { ApiError } from "../utils/ApiError.js";
@@ -36,4 +42,14 @@ router.route("/login").post(loginUser);
 // Secure Routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current—user").get(verifyJWT, getCurrentUser);
+router.route("/update—account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 export default router;
